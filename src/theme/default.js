739d5f8f5698600c8e5520b7104c95762082a144
codexpression-nj <n.codexpression@gmail.com>
2024-07-31 22:35:08 +0200

    update

---
+++ src/theme/default.js
@@ -0,0 +1,15 @@
+export default {
+
+colors: {
+    primary1: "#854CE6",
+    background1: "#222A35",
+    button: "#854CE6",
+    background2: "#19212C",
+    text: "#C8CFD8",
+    text1: "#F2F5F7",
+    text2: "#626970",
+    text3: "#575C66",
+    footerBackground: "#00012B"
+  }
+
+}
